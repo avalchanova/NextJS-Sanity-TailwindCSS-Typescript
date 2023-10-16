ab31338809cf24da3bb9bf7943b68bdf07646b98
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-10-16 16:22:32 +0300

    tailwind

--- next-sanity-app/app/page.tsx
+++ next-sanity-app/app/page.tsx
@@ -3,7 +3,7 @@ import { getProjects } from "@/sanity/sanity-utils"
 export default async function Home() {
     const projects = await getProjects(); // all of this is happening on the server
     //because all of the pages in the app folder are server-side rendered in nextjs13
-  return <div>
+  return <div className="bg-red-500">
         {projects.map((project)=> ( // we would have needed to say that project is of type Project from the types we created, but we did it in the sanity-utils.ts
             <div key={project._id}>
                 {project.name}
--- next-sanity-app/tailwind.config.js
+++ next-sanity-app/tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+     
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+      ], // replaced the whole content [] from the tailwindcss documentation
   theme: {
     extend: {},
   },
