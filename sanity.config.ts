23ab1b60f114197def426e2a044fd0f7217600e0
Aleksandra Adelinova Valchanova <alenceto0@gmail.com>
2023-10-18 00:24:33 +0300

    adding vision plugin

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import schemas from "./sanity/schemas";
+import { visionTool } from "@sanity/vision";
 
 const config = defineConfig({
-    projectId: "3rdvltpc",
-    dataset: 'production',
-    title: 'My Personal Website',
-    apiVersion: '2023-10-16',
-    basePath: '/admin',
-    plugins: [deskTool()],
-    schema: { types: schemas },
+  projectId: "3rdvltpc",
+  dataset: 'production',
+  title: 'My Personal Website',
+  apiVersion: '2023-10-16',
+  basePath: '/admin',
+  plugins: [deskTool(), visionTool()],
+  schema: { types: schemas },
 })
 
 export default config
